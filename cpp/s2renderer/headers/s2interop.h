be887ba2b3fa46da59eda2b327e90871d24964d8
Ren <96073233+Renniren@users.noreply.github.com>
2023-04-29 11:41:11 -0400

    setting up the C++ project will probably revert

---
+++ cpp/s2renderer/headers/s2interop.h
@@ -0,0 +1,17 @@
+#pragma once
+#ifndef S2D_INTEROP_HEADER
+#define S2D_INTEROP_HEADER
+
+
+
+class S2DContext
+{
+
+};
+
+_declspec(dllexport) int interop_test() {
+    return 69;
+}
+
+
+#endif // !S2_INTEROP_HEADER
---
+++ cpp/s2renderer/source files/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+// so we need to send our context from C# over here and allow GL methods
+// to be called from the C# side of the API
+
+int main()
+{
+	using namespace std;
+	cout << "Hello World!" << endl;
+	return 0;
+}
